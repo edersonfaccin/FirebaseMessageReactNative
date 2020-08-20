53a8b2bef68e7f5f3192f40b08a93909838c0420
Ederson Faccin Frasson <edersonfrasson@gmail.com>
2020-08-20 19:48:42 -0300

    Ajustes push

--- services/FCMService.js
+++ services/FCMService.js
@@ -10,8 +10,8 @@ class FCMService {
 
     registerAppWithFCM = async() => {
         if(Platform.OS === 'ios'){
-            await messaging().registerDeviceForRemoteMessagind()
-            await messagind().setAutoInitEnabled(true)
+            await messaging().registerDeviceForRemoteMessages()
+            await messaging().setAutoInitEnabled(true)
         }
     }
 
--- services/LocalNotificationService.js
+++ services/LocalNotificationService.js
@@ -34,7 +34,7 @@ class LocalNotificationService {
         })
     }
 
-    unregister = () => {
+    unRegister = () => {
         PushNotification.unregister()
     }
 
